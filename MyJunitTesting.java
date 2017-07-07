840183522ce02f934f8b446acf8c72bbb476efd5
ROZYTYAGI <tyagirozy4@gmail.com>
2017-07-07 05:01:27 -0700

    Add files via upload

--- MyJunitTesting.java
+++ MyJunitTesting.java
@@ -1,40 +1,46 @@
 package Testing;
 
+import java.io.IOException;
+
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 //import junit.framework.TestCase;
 
 public class MyJunitTesting {
 	EvenTesting jt;
+
 	@Before
-	public void sst()
-	{
+	public void sst() {
 		System.out.println("Tyagi");
-		jt=new EvenTesting();
+		jt = new EvenTesting();
 	}
-	@Ignore
-  public void testCheckEven()
-  {
-	  Assert.assertTrue(jt.checkEven(2));//always start function with test for testing test case
-  }
+
 	@Test
-  public void testCheckEvenNegative()
-  {
-	  Assert.assertTrue(!jt.checkEven(1));
-}
+	public void testCheckEven(){
+		Assert.assertTrue(jt.checkEven(2));// always start function with test for testing test case
+	}
+
 	@Test
-  public void testSum()
-  {
-	  Assert.assertTrue(jt.sum(2, 3)==5);
-}
+	public void testCheckEvenNegative() throws IOException {
+		Assert.assertTrue(!jt.checkEven(1));
+	}
+
+	@Test
+	public void testSum() {
+		Assert.assertTrue(jt.sum(2, 3) == 5);
+	}
+
+	@Test(expected=ArrayIndexOutOfBoundsException.class)
+	public void testArray() {
+		jt.array();
+	}
+
 	@After
-	public void testSecond()
-	{
+	public void testSecond() {
 		System.out.println("Rozy");
-		jt=null;
+		jt = null;
 	}
 }
